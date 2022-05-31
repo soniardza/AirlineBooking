84fa37fdb2ca3c0bc7b598829af4a9fba4b15a3f
sonia-rodriguez-g <sonia.rodriguez@globant.com>
2022-05-31 10:46:10 -0500

    add factory pattern to Flight

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,20 @@ import domain.model.seat.Seat
 import domain.model.seat.SeatClass
 import domain.model.seat.SeatStatus
 import domain.usecases.GetFlights
+import domain.utils.Formatter
+import presentation.PresentationFormat
 import presentation.flight.FlightConsoleFormat
+import presentation.flight.FlightPresentationFactory
 import presentation.ticket.TicketConsoleFormat
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.time.Month
 
 fun main() {
+    val format = PresentationFormat.CONSOLE
+    val flightFormat: Formatter<Flight> = FlightPresentationFactory().getPresentationFormat(format)
+    val flights = GetFlights(flightFormat).invoke()
+    println(flights)
 
-    val getFlights = GetFlights(FlightConsoleFormat())
-    val flightFormat = getFlights.invoke()
-    println(flightFormat)
 
 }
