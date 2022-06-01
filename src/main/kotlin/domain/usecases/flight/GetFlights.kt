08cbe1b69d490c20231a5d069df6040bd9e4ddf6
sonia-rodriguez-g <sonia.rodriguez@globant.com>
2022-06-01 12:19:44 -0500

    modify GetFlights use case and implement dependency injection

--- src/main/kotlin/domain/usecases/flight/GetFlights.kt
+++ src/main/kotlin/domain/usecases/flight/GetFlights.kt
@@ -1,5 +1,6 @@
 package domain.usecases.flight
 
+import domain.datasource.flight.FlightDataSource
 import domain.model.AirCraft
 import domain.model.Airport
 import domain.model.AirportBooking
@@ -12,42 +13,14 @@ import java.time.Month
 /**
  * 1. Mostrar los vuelos disponibles de un mes
  **/
-class GetFlights {
-    fun invoke(month: Month): Map<Int, Flight> {
-        val flight = Flight(
-            number = "Y4 708",
-            airCraft = AirCraft("Airbus", "A320"),
-            price = BigDecimal(100.0),
-            departureArrivalBooking = getAirportPair()
-        )
+class GetFlights(
+    private val flightDataSource: FlightDataSource
+) {
 
-        val flightsMap = mapOf(
-            1 to flight,
-            2 to flight,
-            3 to flight
-        )
-
-        return flightsMap.filter { flightEntry ->
-            flightEntry.value.departureArrivalBooking.first.dateTime.month == month
-        }
-
-
-    }
-
-    private fun getAirportPair(): Pair<AirportBooking, AirportBooking> {
-        return Pair(
-            AirportBooking(
-                airport = Airport("BOG", "Bogotá"),
-                dateTime = LocalDateTime.of(
-                    2023, Month.JANUARY, 10, 13, 0, 0
-                )
-            ),
-            AirportBooking(
-                airport = Airport("CUN", "Cancún"),
-                dateTime = LocalDateTime.of(
-                    2023, Month.JANUARY, 10, 17, 30, 0
-                )
-            )
-        )
-    }
+    fun invoke(month: Month): Map<Int, Flight> =
+        flightDataSource
+            .getFlights()
+            .filter { flightEntry ->
+                flightEntry.value.departureArrivalBooking.first.dateTime.month == month
+            }
 }
